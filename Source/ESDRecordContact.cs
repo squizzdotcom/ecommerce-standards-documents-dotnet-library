d27e4ce627a46f448aae4b1bdf571a9d6840e93b
squizzdev <rowan.c.drew@squizz.com>
2015-12-23 19:37:02 +1100

    Added contact record class to store details of a contact. Added document classes to store records of contacts associated to customer accounts and supplier accounts.

---
+++ Source/ESDRecordContact.cs
@@ -0,0 +1,83 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce Standards record that contains data for a single contact. A contact contains information about a person on how they can be communicated with. The contact record may be linked to a customer account, or supplier account</summary>
+    [DataContract]
+    public class ESDRecordContact
+    {
+        /// <summary>Key that allows the contact record to be uniquely identified and linked to.</summary>
+        [DataMember]
+        public string keyContactID { get; set; }
+
+        /// <summary>Key of the customer account record that the contact record is linked to.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyCustomerAccountID { get; set; }
+
+        /// <summary>Key of the supplier account record that the contact record is linked to.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keySupplierAccountID { get; set; }
+
+        /// <summary>Code of the contact. May or may not be a unqiue identifier.</summary>
+        [DataMember]
+        public string contactCode { get; set; }
+
+        /// <summary>Title of the contact person. Eg. Mrs, Mr, Miss, Doctor</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string title { get; set; }
+
+        /// <summary>First name field of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string name1 { get; set; }
+
+        /// <summary>Second name field of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string name2 { get; set; }
+
+        /// <summary>Third name field of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string name3 { get; set; }
+
+        /// <summary>Email address of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string email { get; set; }
+
+        /// <summary>First phone number of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string phone1 { get; set; }
+
+        /// <summary>Second phone number of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string phone2 { get; set; }
+
+        /// <summary>Third phone number of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string phone3 { get; set; }
+
+        /// <summary>fax number of the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string fax { get; set; }
+
+        /// <summary>URL of the website that the contact is associated to.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string website { get; set; }
+
+        /// <summary>Name of the organisation associated to the contact.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string orgName { get; set; }
+
+        /// <summary>Name of the employment position of the contact within the organisation.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string orgPosition { get; set; }
+    }
+}
---
+++ Source/ESDocumentCustomerAccountContact.cs
@@ -0,0 +1,79 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>
+    /// Ecommerce standards document that holds a list of customer account records
+    /// </summary>
+    /// <example>
+    /// An example of the Customer Account Ecommerce Standards document in its JSON serialised form
+    /// <code>
+    /// {
+    ///     "resultStatus":"1",
+    ///     "message":"The customer account contact data has been successfully obtained.",
+    ///     "configs":{"dataFields":"keyContactID,keyCustomerAccountID,contactCode,title,name1,name2,name3,email,phone1,phone2,phone3,fax,website,orgName,orgPosition"},
+    ///     "dataTransferMode": "COMPLETE",
+    ///     "version": 1.0,
+    ///     "totalDataRecords": 2,
+    ///     "dataRecords":
+    ///      [
+    ///         {
+    ///             "keyContactID":"CON1",
+    ///         },
+    ///         {
+    ///             "keyContactID":"2",
+    ///             "keyCustomerAccountID":"222",
+    ///             "contactCode":"JD123",
+    ///             "title":"Mr",
+    ///             "name1":"John",
+    ///             "name2":"Wilbur",
+    ///             "name3":"Smith",
+    ///             "email":"personal@test.com",
+    ///             "phone1":"+6112341231",
+    ///             "phone2":"+6112341232",
+    ///             "phone3":"+6112341233",
+    ///             "fax":"+6112341234",
+    ///             "website":"www.squizz.com/esd/index.html",
+    ///             "orgName":"Squizz Pty Ltd",
+    ///             "orgPosition":"Service Centre Coordinator"
+    ///         }
+    ///     ]
+    /// }
+    /// </code>
+    /// </example>
+    [DataContract]
+    class ESDocumentCustomerAccountContact : ESDocument
+    {
+        /// <summary>List of contact records</summary>
+        [JsonProperty(Order = -4)]
+        [DataMember]
+        public ESDRecordContact[] dataRecords;
+
+        /// <summary>Constructor</summary>
+        /// <param name="resultStatus">status of obtaining the customer account contact record data</param>
+        /// <param name="message">message to accompany the result status</param>
+        /// <param name="contactRecords">list of contact records</param>
+        /// <param name="configs">A list of key value pairs that contain additional information about the document.
+        /// Ensure that a key "dataFields" exists that contains a comma delimited list of the contact record properties that have data set. This advises systems processing the data which properties should be read and have defaults set if not included in each record.
+        /// </param>
+        public ESDocumentCustomerAccountContact(int resultStatus, string message, ESDRecordContact[] contactRecords, Dictionary<string, string> configs)
+        {
+            this.resultStatus = resultStatus;
+            this.message = message;
+            this.dataRecords = contactRecords;
+            this.configs = configs;
+            this.totalDataRecords = contactRecords.Length;
+        }
+    }
+}
