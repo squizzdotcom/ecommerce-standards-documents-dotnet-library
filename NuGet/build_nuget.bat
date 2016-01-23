aa8b53e2686618d428edcdd7383365c5ebed490f
Adam Stiskala <adam@readysell.com.au>
2016-01-23 11:36:28 +1100

    Fix NuGet build script

--- NuGet/build_nuget.bat
+++ NuGet/build_nuget.bat
@@ -4,8 +4,9 @@ ECHO Ensure a 'Release' build of the library has been completed and that
 ECHO the Package.nuspec file has been updated with the correct version
 PAUSE
 
+mkdir EcommerceStandardsDocuments\lib\net4\
 copy ..\Source\bin\Release\EcommerceStandardsDocuments.dll EcommerceStandardsDocuments\lib\net4\EcommerceStandardsDocuments.dll
-NuGet.exe pack EcommerceStandardsDocuments\Package.nuspec
+NuGet.exe pack EcommerceStandardsDocuments\EcommerceStandardsDocuments.nuspec
 
 ECHO Generated NuGet package can now be pushed to repository
 PAUSE
