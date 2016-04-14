21928c4fa81a1f99fafdd58424e4c2f61de30368
Rowan Drew <rowan.c.drew@squizz.com>
2016-04-14 18:16:17 +1000

    Fixes #26
    Created classes for storing individual item relation records and storing records in a document class.

---
+++ Source/ESDRecordItemRelation.cs
@@ -0,0 +1,45 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce Standards Record that holds data for a single relation between items. Each record links one product, labour, or download record to another product, labour, or download record</summary>
+    [DataContract]
+    public class ESDRecordItemRelation
+    {
+        /// <summary>Key of the product record</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyProductID { get; set; }
+        /// <summary>Key of the download record</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyDownloadID { get; set; }
+        /// <summary>Key of the labour record</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyLabourID { get; set; }
+        /// <summary>Key of the product record that is related</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyRelatedProductID { get; set; }
+        /// <summary>Key of the download record that is related</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyRelatedDownloadID { get; set; }
+        /// <summary>Key of the labour record that is related</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyRelatedLabourID { get; set; }
+        /// <summary>Data Record OPeration. Denotes an operation that may need to be performed on the record when it is being processed. 
+        /// Set null, or set it to one of the ESD_RECORD_OPERATION constants in the ESDocumentConstants class to allow the record to be inserted, updated, deleted, or ignored.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int drop { get; set; }
+        /// <summary>Stores an identifier that is relevant only to the system referencing and storing the record for its own needs.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string internalID { get; set; }
+    }
+}
---
+++ Source/ESDocumentItemRelation.cs
@@ -0,0 +1,76 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce standards document that contains a list of item relation records</summary>
+    /// <example>
+    /// An example of the item relation Ecommerce Standards document in its JSON serialised form
+    /// <code>
+    /// {
+    ///     "resultStatus":"1",
+    ///     "message":"The item relation data has been successfully obtained.",
+    ///     "configs":{"dataFields":"keyProductID,keyDownloadID,keyLabourID,keyRelatedDownloadID,keyRelatedProductID,keyRelatedLabourID"},
+    ///     "dataTransferMode": "COMPLETE",
+    ///     "version": 1.0,
+    ///     "totalDataRecords": 4,
+    ///     "dataRecords":
+    ///      [
+    ///         {
+    ///             "keyProductID":"123",
+    ///             "keyRelatedProductID":"456"
+    ///         },
+    ///         {
+    ///             "keyProductID":"123",
+    ///             "keyRelatedDownloadID": "DWN1"
+    ///         },
+    ///         {
+    ///             "keyDownloadID":"DWN1",
+    ///             "keyRelatedLabourID": "LAB2"
+    ///         },
+    ///         {
+    ///             "keyLabourID":"LAB1",
+    ///             "keyRelatedProductID": "123"
+    ///         }
+    ///     ]
+    /// }
+    /// </code>
+    /// </example>
+    [DataContract]
+    public class ESDocumentItemRelation : ESDocument
+    {
+        /// <summary>List of item relation records</summary>
+        [JsonProperty(Order = -4)]
+        [DataMember]
+        public ESDRecordItemRelation[] dataRecords;
+
+        /// <summary>Constructor</summary>
+        /// <param name="resultStatus">status of obtaining the item relation data</param>
+        /// <param name="message">message to accompany the result status</param>
+        /// <param name="itemRelationRecords">list of item relation records</param>
+        /// <param name="configs">A list of key value pairs that contain additional information about the document.
+        /// Ensure that a key "dataFields" exists that contains a comma delimited list of the item relation record properties that have data set. This advises systems processing the data which properties should be read and have defaults set if not included in each record.
+        /// </param>
+        public ESDocumentItemRelation(int resultStatus, string message, ESDRecordItemRelation[] itemRelationRecords, Dictionary<string, string> configs)
+        {
+            this.resultStatus = resultStatus;
+            this.message = message;
+            this.dataRecords = itemRelationRecords;
+            this.configs = configs;
+            if (itemRelationRecords != null)
+            {
+                this.totalDataRecords = itemRelationRecords.Length;
+            }
+        }
+    }
+}
