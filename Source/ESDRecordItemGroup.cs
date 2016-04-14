fcd30c9f9119904438cb71bbf89b5f48d794c140
Rowan Drew <rowan.c.drew@squizz.com>
2016-04-14 18:15:33 +1000

    Fixes #27
    Created classes for store individual item group records and storing records in a document class.

---
+++ Source/ESDRecordItemGroup.cs
@@ -0,0 +1,59 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce Standards Record that holds data for a single item group. Each record represents a group of assigned products, downloads and/or labour.</summary>
+    [DataContract]
+    public class ESDRecordItemGroup
+    {
+        /// <summary>Key of the item Group record</summary>
+        [DataMember]
+        public string keyItemGroupID { get; set; }
+        /// <summary>Code of the group, may or may not be a unique identifier.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string groupCode { get; set; }
+        /// <summary>Label of item group.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string groupLabel { get; set; }
+        /// <summary>Description of the item group.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string groupDescription { get; set; }
+        /// <summary>List of key product record IDs that link product records to the group</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string[] keyProductIDs { get; set; }
+        /// <summary>List of key download IDs that link download records to the group</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string[] keyDownloadIDs { get; set; }
+        /// <summary>List of key labour IDs that link labour records to the group</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string[] keyLabourIDs { get; set; }
+
+        /// <summary>Key product ID that denotes one product record in the group that is the default or primary representative of the group.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyDefaultProductID { get; set; }
+        /// <summary>Key download ID that denotes one download record in the group that is the default or primary representative of the group.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyDefaultDownloadID { get; set; }
+        /// <summary>Key labour ID that denotes one labour record in the group that is the default or primary representative of the group.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string keyDefaultLabourID { get; set; }
+
+        /// <summary>Data Record OPeration. Denotes an operation that may need to be performed on the record when it is being processed. 
+        /// Set null, or set it to one of the ESD_RECORD_OPERATION constants in the ESDocumentConstants class to allow the record to be inserted, updated, deleted, or ignored.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int drop { get; set; }
+        /// <summary>Stores an identifier that is relevant only to the system referencing and storing the record for its own needs.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string internalID { get; set; }
+    }
+}
---
+++ Source/ESDocumentItemGroup.cs
@@ -0,0 +1,87 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce standards document that contains a list of item group records</summary>
+    /// <example>
+    /// An example of the item group Ecommerce Standards document in its JSON serialised form
+    /// <code>
+    /// {
+    ///     "resultStatus":"1",
+    ///     "message":"The item group data has been successfully obtained.",
+    ///     "configs":{"dataFields":"keyItemGroupID,groupCode,groupLabel,groupDescription,keyDefaultProductID,keyDefaultDownloadID,keyDefaultLabourID"},
+    ///     "dataTransferMode": "COMPLETE",
+    ///     "version": 1.0,
+    ///     "totalDataRecords": 3,
+    ///     "dataRecords":
+    ///      [
+    ///         {
+    ///             "keyItemGroupID":"1"
+    ///         },
+    ///         {
+    ///             "keyItemGroupID":"2",
+    ///             "groupCode": "COL-TEATOWEL",
+    ///             "groupLabel": "Coloured Teatowels",
+    ///             "groupDescription": "Group contains an assorted number of teatowels, each styled with vibrant colours.",
+    ///             "keyDefaultProductID": "456",
+    ///             "keyDefaultDownloadID": "",
+    ///             "keyDefaultLabourID": ""
+    ///             "keyProductIDs": ["123","456","789","1000"],
+    ///             "keyDownloadIDs": [],
+    ///             "keyLabourIDs": []
+    ///         },
+    ///         {
+    ///             "keyItemGroupID":"3",
+    ///             "groupCode": "GRP-TV-SETUP",
+    ///             "groupLabel": "Amazing TV Bundle",
+    ///             "groupDescription": "Bundle contains everything you need to get a TV. It contains a television, labour to install the TV, and a downloadable manual to be able to use the TV.",
+    ///             "keyDefaultProductID": "0987",
+    ///             "keyDefaultDownloadID": "",
+    ///             "keyDefaultLabourID": ""
+    ///             "keyProductIDs": ["0987"],
+    ///             "keyDownloadIDs": ["DOWN-9898"],
+    ///             "keyLabourIDs": ["LAB-INSTALL"]
+    ///         }
+    ///     ]
+    /// }
+    /// </code>
+    /// </example>
+    [DataContract]
+    public class ESDocumentItemGroup : ESDocument
+    {
+        /// <summary>List of item group records</summary>
+        [JsonProperty(Order = -4)]
+        [DataMember]
+        public ESDRecordItemGroup[] dataRecords;
+
+        /// <summary>Constructor</summary>
+        /// <param name="resultStatus">status of obtaining the item group data</param>
+        /// <param name="message">message to accompany the result status</param>
+        /// <param name="itemGroupRecords">list of item group records</param>
+        /// <param name="configs">A list of key value pairs that contain additional information about the document.
+        /// Ensure that a key "dataFields" exists that contains a comma delimited list of the item group record properties that have data set. This advises systems processing the data which properties should be read and have defaults set if not included in each record.
+        /// </param>
+        public ESDocumentItemGroup(int resultStatus, string message, ESDRecordItemGroup[] itemGroupRecords, Dictionary<string, string> configs)
+        {
+            this.resultStatus = resultStatus;
+            this.message = message;
+            this.dataRecords = itemGroupRecords;
+            this.configs = configs;
+            if (itemGroupRecords != null)
+            {
+                this.totalDataRecords = itemGroupRecords.Length;
+            }
+        }
+    }
+}
