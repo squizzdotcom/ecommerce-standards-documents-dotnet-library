8cb19af8477d7372c577c7cc12a67a0b134d652e
Rowan Drew <rowan.c.drew@squizz.com>
2016-04-14 18:28:34 +1000

    Fixes #24
    Added class to store details of each surcharge record, as well as a document class to store a list of records.

---
+++ Source/ESDRecordSurcharge.cs
@@ -0,0 +1,42 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce Standards Record that holds data for a single surcharge. Each surcharge defines a type of monetary amount that may be added or substracted which is factored into the final price of purchasing goods or services.</summary>
+    [DataContract]
+    public class ESDRecordSurcharge
+    {
+        /// <summary>Key of the surcharge record to be uniquely identified and linked to.</summary>
+        [DataMember]
+        public string keySurchargeID { get; set; }
+        /// <summary>Code of the surcharge.  May or may not be a unique identifier</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string surchargeCode { get; set; }
+        /// <summary>Label of the surcharge</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string surchargeLabel { get; set; }
+        /// <summary>Text to describe the surcharge</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string description { get; set; }
+        /// <summary>Type of surcharge that it relates to. Set null, or set it to ine of the SURCHARGE_TYPE constants in the ESDocumentConstants class.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string surchargeType { get; set; }
+        /// <summary>Data Record OPeration. Denotes an operation that may need to be performed on the record when it is being processed. 
+        /// Set null, or set it to one of the ESD_RECORD_OPERATION constants in the ESDocumentConstants class to allow the record to be inserted, updated, deleted, or ignored.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int drop { get; set; }
+        /// <summary>Stores an identifier that is relevant only to the system referencing and storing the record for its own needs.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string internalID { get; set; }
+    }
+}
---
+++ Source/ESDocumentSurcharge.cs
@@ -0,0 +1,84 @@
+﻿/// <remarks>
+/// Copyright (C) 2016 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce standards document that contains a list of surcharge records</summary>
+    /// <example>
+    /// An example of the Surcharge Ecommerce Standards document in its JSON serialised form
+    /// <code>
+    /// {
+    ///     "resultStatus":"1",
+    ///     "message":"The surcharge data has been successfully obtained.",
+    ///     "configs":{"dataFields":"keySurchargeID,surchargeCode,surchargeLabel,description,surchargeType"},
+    ///     "dataTransferMode": "COMPLETE",
+    ///     "version": 1.0,
+    ///     "totalDataRecords": 4,
+    ///     "dataRecords":
+    ///      [
+    ///         {
+    ///             "keySurchargeID":"123"
+    ///         },
+    ///         {
+    ///             "keySurchargeID":"456",
+    ///             "surchargeCode": "WEB_FREIGHT",
+    ///             "surchargeLabel":"Web Order Freight",
+    ///             "description": "Costs to deliver orders created through the website.",
+    ///             "surchargeType": "FREIGHT"
+    ///         },
+    ///         {
+    ///             "keySurchargeID":"765",
+    ///             "surchargeCode": "WEB_CC_SURCHARGE",
+    ///             "surchargeLabel": "Web Credit Card Surcharge",
+    ///             "description": "Transaction cost for payment made by credit card.",
+    ///             "surchargeType": "CREDITCARD"
+    ///         },
+    ///         {
+    ///             "keySurchargeID":"6765",
+    ///             "surchargeCode": "WEB_MIN_ORDER",
+    ///             "surchargeLabel": "Web Minimum Order Surcharge",
+    ///             "description": "Cost to handle orders when an order's total price is under the allowed minimum.",
+    ///             "surchargeType": "MINORDER"
+    ///         }
+    ///     ]
+    /// }
+    /// </code>
+    /// </example>
+    [DataContract]
+    public class ESDocumentSurcharge : ESDocument
+    {
+        /// <summary>List of surcharge records</summary>
+        [JsonProperty(Order = -4)]
+        [DataMember]
+        public ESDRecordSurcharge[] dataRecords;
+
+        /// <summary>Constructor</summary>
+        /// <param name="resultStatus">status of obtaining the surcharge data</param>
+        /// <param name="message">message to accompany the result status</param>
+        /// <param name="surchargeRecords">list of surcharge records</param>
+        /// <param name="configs">A list of key value pairs that contain additional information about the document.
+        /// Ensure that a key "dataFields" exists that contains a comma delimited list of the surcharge record properties that have data set. This advises systems processing the data which properties should be read and have defaults set if not included in each record.
+        /// </param>
+        public ESDocumentSurcharge(int resultStatus, string message, ESDRecordSurcharge[] surchargeRecords, Dictionary<string, string> configs)
+        {
+            this.resultStatus = resultStatus;
+            this.message = message;
+            this.dataRecords = surchargeRecords;
+            this.configs = configs;
+            if (surchargeRecords != null)
+            {
+                this.totalDataRecords = surchargeRecords.Length;
+            }
+        }
+    }
+}
