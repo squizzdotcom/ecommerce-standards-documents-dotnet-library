a564e3d6f4795d89b41dc0f857fe38d66c3759de
Rowan Drew <rowan.c.drew@squizz.com>
2016-05-19 20:40:39 +1000

    Fixes #36
    Added keyTaxcodeID property to ESDocumentSurcharge class to be able to assign a taxcode to the surcharge.

--- Source/ESDRecordSurcharge.cs
+++ Source/ESDRecordSurcharge.cs
@@ -31,6 +31,15 @@ namespace EcommerceStandardsDocuments
         /// <summary>Type of surcharge that it relates to. Set null, or set it to ine of the SURCHARGE_TYPE constants in the ESDocumentConstants class.</summary>
         [DataMember(EmitDefaultValue = false)]
         public string surchargeType { get; set; }
+        /// <summary>Key of the Taxcode record that the surcharge is assigned to. The taxcode may control the amount of tax applied to the surcharge.</summary>
+        [DataMember]
+        public string keyTaxcodeID { get; set; }
+        /// <summary>Either 'N'-No or 
+        /// 'Y'-Yes
+        /// If 'Y' then indicates that the surcharge should always be included with sales orders and purchase orders even if it has 0 monetary amount applied to the order
+        /// </summary>
+        [DataMember]
+        public string alwaysShowInOrders { get; set; }
         /// <summary>Data Record OPeration. Denotes an operation that may need to be performed on the record when it is being processed. 
         /// Set null, or set it to one of the ESD_RECORD_OPERATION constants in the ESDocumentConstants class to allow the record to be inserted, updated, deleted, or ignored.</summary>
         [DataMember(EmitDefaultValue = false)]
