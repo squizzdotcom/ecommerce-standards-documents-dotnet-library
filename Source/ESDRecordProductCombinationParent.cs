b5fd6bcaa21fe0784afe5c2049db1cfd65a668ce
Rowan Drew <rowan.c.drew@squizz.com>
2017-03-07 18:06:54 +1100

    Fixes #46
    --ProductCombinationParent.cs
    Modified defaultCombination property to set its default value to -1 ensuring that if its value is set to 0 then it denotes that the first child product assigned to the parent product in the combination is the default.

--- Source/ESDRecordProductCombinationParent.cs
+++ Source/ESDRecordProductCombinationParent.cs
@@ -1,5 +1,5 @@
 ﻿/// <remarks>
-/// Copyright (C) 2016 Squizz PTY LTD
+/// Copyright (C) 2017 Squizz PTY LTD
 /// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 /// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 /// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using System.ComponentModel;
 
 namespace EcommerceStandardsDocuments
 {
@@ -16,6 +18,16 @@ namespace EcommerceStandardsDocuments
     [DataContract]
     public class ESDRecordProductCombinationParent
     {
+        /// <summary>For product combinations specifics the value to set when no child product is the default within the combination</summary>
+        public const int DEFAULT_COMBINATION_NOT_SET = -1;
+
+        /// <summary>Constructor</summary>
+        public ESDRecordProductCombinationParent()
+        {
+            //denote that the default product combination has not been set
+            defaultCombination = DEFAULT_COMBINATION_NOT_SET;
+        }
+
         /// <summary>Key of the product record that links it to being the parent product of the combination</summary>
         [DataMember]
         public string keyProductID { get; set; }
@@ -33,7 +45,8 @@ namespace EcommerceStandardsDocuments
         [DataMember]
         public ESDRecordProductCombination[] productCombinations { get; set; }
         /// <summary>Index of the product combinations list that denotes the combination that is the default. The default may be set when initially viewing a combination product</summary>
-        [DataMember(EmitDefaultValue = false)]
+        [DefaultValue(-1)]
+        [DataMember(EmitDefaultValue = false)]        
         public int defaultCombination { get; set; }
     }
 }
