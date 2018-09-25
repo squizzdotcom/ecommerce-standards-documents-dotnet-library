08e0a028f802502a1fd8745d7f6410b031deef47
Rowan Drew <rowan.c.drew@squizz.com>
2018-09-25 20:15:04 +1000

    Fixes #67
    --ESDRecordCurrencyExchangeRate.cs
    Created record class to store the details of an exchange rate between 2 currencies.
    
    --ESDocumentCurrencyExchangeRate.cs
    Created document class to store a list of currency exchange rates.

---
+++ Source/ESDRecordCurrencyExchangeRate.cs
@@ -0,0 +1,60 @@
+﻿/// <remarks>
+/// Copyright (C) 2018 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce Standards Record that holds data for a single currency exchange rate that reflects the cost of purchasing one currency from another. These rates may be used to inflience the price of selling or buying goods and services between states or countries.</summary>
+    [DataContract]
+    public class ESDRecordCurrencyExchangeRate
+    {
+        /// <summary>Key that allows the currency exchange rate record to be uniquely identified. The value set may or may not be a human known identifier</summary>
+        [DataMember]
+        public string keyCurrencyExchangeRateID { get; set; }
+
+        /// <summary>The code of the currency being sold. This currency code may conform to the 3 digit ISO 4217 currency code standards, or crypto currency code standards.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string sellCurrencyCode { get; set; }
+
+        /// <summary>Name of the currency being sold.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string sellCurrencyName { get; set; }
+
+        /// <summary>Specifies the kind of currency that is being sold, such as a cryptographic, or decentralised currency, as opposed to a state/country based currency controlled by government (fiat). If set must use the ESDocumentConstants class properties that are prefixed with "CURRENCY_TYPE_"</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string sellCurrencyType { get; set; }
+
+        /// <summary>The code of the currency being purchased. This currency code may conform to the 3 digit ISO 4217 currency code standards, or crypto currency code standards.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string buyCurrencyCode { get; set; }
+
+        /// <summary>Name of the currency being purchased.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string buyCurrencyName { get; set; }
+
+        /// <summary>Specifies the kind of currency that is being purchased, such as a cryptographic, or decentralised currency, as opposed to a state/country based currency controlled by government (fiat).  If set must use the ESDocumentConstants class properties that are prefixed with "CURRENCY_TYPE_"</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string buyCurrencyType { get; set; }
+
+        /// <summary>The percentage rate used determine how much its costs to sell 1 of the selling currency in the purchasing currency. The percentage rate needs to be set as a decimal eg. for a rate of 70% the value 0.70 should be set. To determine the cost to buy currency being sold use the equation (X sellCurrency x exchangeRate) = Y buyCurrency</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public decimal exchangeRate { get; set; }
+
+        /// <summary>Data Record OPeration. Denotes an operation that may need to be performed on the record when it is being processed. 
+        /// Set null, or  it to one of the ESD_RECORD_OPERATION constants in the ESDocumentConstants class to allow the record to be inserted, updated, deleted, or ignored.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int drop { get; set; }
+
+        /// <summary>Stores an identifier that is relevant only to the system referencing and storing the record for its own needs.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string internalID { get; set; }
+    }
+}
---
+++ Source/ESDocumentCurrencyExchangeRate.cs
@@ -0,0 +1,109 @@
+﻿/// <remarks>
+/// Copyright (C) 2018 Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>
+    /// Ecommerce standards document that holds a list of currency exchange rate records
+    /// </summary>
+    /// <example>
+    /// An example of the currency exchange rate Ecommerce Standards document in its JSON serialised form
+    /// <code>
+    /// {
+    ///     "version": 1.3,
+    ///     "resultStatus": 1,
+    ///     "message":"The currency exchange rate data has been successfully obtained.",
+    ///     "dataTransferMode": "COMPLETE",
+    ///     "totalDataRecords": 4,
+    ///     "configs":{"dataFields":"keyCurrencyExchangeRateID,sellCurrencyISOCode,sellCurrencyName,sellCurrencyType,buyCurrencyISOCode,buyCurrencyName,buyCurrencyType,exchangeRate"},
+    ///     "dataRecords":
+    ///      [
+    ///         {
+    ///             "keyCurrencyExchangeRateID":"1"
+    ///         },
+    ///         //exchange rate from Australian Dollars to US dollars, $1 AUD sells for $0.70 USD
+    ///         {
+    ///             "keyCurrencyExchangeRateID":"2",
+    ///             "sellCurrencyISOCode":"AUD",
+    ///             "sellCurrencyName":"Australian Dollars",
+    ///             "sellCurrencyType":"FIAT",
+    ///             "buyCurrencyCode":"USD",
+    ///             "buyCurrencyName":"United States Dollars",
+    ///             "buyCurrencyType":"FIAT",
+    ///             "exchangeRate": 0.7000
+    ///         },
+    ///         //exchange rate USD to AUD, $1 USD sells for $1.30 AUD
+    ///         {
+    ///             "keyCurrencyExchangeRateID":"3",
+    ///             "sellCurrencyISOCode":"USD",
+    ///             "sellCurrencyName":"United States Dollars",
+    ///             "sellCurrencyType":"FIAT",
+    ///             "buyCurrencyCode":"AUD",
+    ///             "buyCurrencyName":"Australian Dollars",
+    ///             "buyCurrencyType":"FIAT",
+    ///             "exchangeRate": 1.3000
+    ///         },
+    ///         //exchange rate USD to EUR, $1 USD sells for €0.7727 EUR
+    ///         {
+    ///             "keyCurrencyExchangeRateID":"4AB",
+    ///             "sellCurrencyISOCode":"USD",
+    ///             "sellCurrencyName":"United States Dollars",
+    ///             "sellCurrencyType":"FIAT",
+    ///             "buyCurrencyCode":"EUR",
+    ///             "buyCurrencyName":"Euros",
+    ///             "buyCurrencyType":"FIAT",
+    ///             "exchangeRate": 0.7727
+    ///         },
+    ///         //exchange rate Bitcoin to US Dollars, 1 Bitcoin sells for $8023.60 US Dollars
+    ///         {
+    ///             "keyCurrencyExchangeRateID":"5CC",
+    ///             "sellCurrencyISOCode":"BTC",
+    ///             "sellCurrencyName":"Bitcoins",
+    ///             "sellCurrencyType":"CRYPTO",
+    ///             "buyCurrencyCode":"USD",
+    ///             "buyCurrencyName":"United States Dollars",
+    ///             "buyCurrencyType":"FIAT",
+    ///             "exchangeRate": 8023.6000
+    ///         }
+    ///     ]
+    /// }
+    /// </code>
+    /// </example>
+    [DataContract]
+    public class ESDocumentCurrencyExchangeRate : ESDocument
+    {
+        /// <summary>List of currency exchange rate records</summary>
+        [JsonProperty(Order = -4)]
+        [DataMember]
+        public ESDRecordCurrencyExchangeRate[] dataRecords;
+
+        /// <summary>Constructor</summary>
+        /// <param name="resultStatus">status of obtaining the currency exchange rate record data</param>
+        /// <param name="message">message describing the status of obtaining the data for the document</param>
+        /// <param name="currencyExchangeRateRecords">list of currency exchange rate records</param>
+        /// <param name="configs">A list of key value pairs that contain additionally information about the document.
+        /// Ensure that a key "dataFields" exists that contains a comma delimited list of the currency exchange rate record properties that have data set. This advises systems processing the data which properties should be read and have defaults set if not included in each record.
+        /// </param>
+        public ESDocumentCurrencyExchangeRate(int resultStatus, string message, ESDRecordCurrencyExchangeRate[] currencyExchangeRateRecords, Dictionary<string, string> configs)
+        {
+            this.resultStatus = resultStatus;
+            this.message = message;
+            this.dataRecords = currencyExchangeRateRecords;
+            this.configs = configs;
+            if (currencyExchangeRateRecords != null)
+            {
+                this.totalDataRecords = currencyExchangeRateRecords.Length;
+            }
+        }
+    }
+}
