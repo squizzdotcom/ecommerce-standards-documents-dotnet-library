9895c47fc08a69f7d72f30b8f65e8525e0afb196
Rowan Drew <rowan.c.drew@squizz.com>
2021-04-07 15:52:30 +1000

    Fixes #72
    --ESDRecordCategoryTree.cs
    Added class to represent a Category Tree.
    
    --ESDocuemntCategory.cs
    Modified class to store a list of category tree records against the document. Updated JSON examples to include category tree data.

---
+++ Source/ESDRecordCategoryTree.cs
@@ -0,0 +1,48 @@
+﻿/// <remarks>
+/// Copyright (C) Squizz PTY LTD
+/// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+/// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
+/// </remarks>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace EcommerceStandardsDocuments
+{
+    /// <summary>Ecommerce Standards record that contains data for a single category tree. A category tree represents an overall hierarchical collection of categories, used to navigate and find associated products, maker models etc.. assigned to each category.</summary>
+    [DataContract]
+    public class ESDRecordCategoryTree
+    {
+        /// <summary>Key that allows the category record to be uniquely identified and linked to.</summary>
+        [DataMember]
+        public string keyCategoryTreeID { get; set; }
+
+        /// <summary>Code of the category tree. May or may not be a unqiue identifier.</summary>
+        [DataMember]
+        public string categoryTreeCode { get; set; }
+
+        /// <summary>Name of the category tree.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string name { get; set; }
+
+        /// <summary>Description field of the category tree. It may contain any text used to describe the category tree.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string description { get; set; }
+
+        /// <summary>Number to order the category tree by. This may be used to order a number of category trees that appear within a list.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int ordering { get; set; }
+
+        /// <summary>Data Record OPeration. Denotes an operation that may need to be performed on the record when it is being processed. 
+        /// Set null, or set it to one of the ESD_RECORD_OPERATION constants in the ESDocumentConstants class to allow the record to be inserted, updated, deleted, or ignored.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public int drop { get; set; }
+
+        /// <summary>Stores an identifier that is relevant only to the system referencing and storing the record for its own needs.</summary>
+        [DataMember(EmitDefaultValue = false)]
+        public string internalID { get; set; }
+    }
+}
--- Source/ESDocumentCategory.cs
+++ Source/ESDocumentCategory.cs
@@ -1,5 +1,5 @@
 ﻿/// <remarks>
-/// Copyright (C) 2019 Squizz PTY LTD
+/// Copyright (C) Squizz PTY LTD
 /// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 /// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 /// You should have received a copy of the GNU General Public License along with this program.  If not, see http://www.gnu.org/licenses/.
@@ -14,18 +14,35 @@ using Newtonsoft.Json;
 namespace EcommerceStandardsDocuments
 {
     /// <summary>
-    /// Ecommerce standards document that contains a list of category records
+    /// Ecommerce standards document that contains a list of category records, and a list of category tree records
     /// </summary>
     /// <example>
     /// An example of the Category Ecommerce Standards document in its JSON serialised form
     /// <code>
     /// {
-    ///     "version": 1.3,
+    ///     "version": 1.4,
     ///     "resultStatus": 1,
     ///     "message":"The category data has been successfully obtained.",
     ///     "dataTransferMode": "COMPLETE",
     ///     "totalDataRecords": 3,
-    ///     "configs":{"dataFields":"keyCategoryID,categoryCode,keyCategoryParentID,name,description1,description2,description3,description4,metaTitle,metaKeywords,metaDescription,ordering"},
+    ///     "configs":{
+    ///         "dataFieldsCategoryTree":"keyCategoryTreeID,categoryTreeCode,name,description,ordering",
+    ///         "dataFields":"keyCategoryID,categoryCode,keyCategoryParentID,name,description1,description2,description3,description4,metaTitle,metaKeywords,metaDescription,ordering"
+    ///     },
+    ///     "categoryTreeRecords":
+    ///     [
+    ///         {
+    ///             "keyCategoryTreeID":"1",
+    ///             "name":"Product Catalogue",
+    ///         },
+    ///         {
+    ///             "keyCategoryTreeID":"2-BRANDS",
+    ///             "categoryTreeCode":"BRANDS",
+    ///             "name":"Product Brands",
+    ///             "description":"View all the brands of products we sell.",
+    ///             "ordering":2
+    ///         }
+    ///     ],
     ///     "dataRecords":
     ///      [
     ///         {
@@ -35,6 +52,7 @@ namespace EcommerceStandardsDocuments
     ///         {
     ///             "keyCategoryID":"123",
     ///             "categoryCode":"tables-chairs",
+    ///             "keyCategoryTreeID":"1",
     ///             "keyCategoryParentID":"2",
     ///             "name":"Tables and Chairs",
     ///             "description1":"View our extensive range of tables and chairs",
@@ -52,6 +70,7 @@ namespace EcommerceStandardsDocuments
     ///         {
     ///             "keyCategoryID":"124",
     ///             "categoryCode":"paper",
+    ///             "keyCategoryTreeID":"1",
     ///             "keyCategoryParentID":"2",
     ///             "name":"Paper Products",
     ///             "description1":"View our extensive range of paper",
@@ -65,6 +84,24 @@ namespace EcommerceStandardsDocuments
     ///             "keyProductIDs":["PROD-001","PROD-002"],
     ///             "keyDownloadIDs":["DOWN-1", "DOWN-2","DOWN-3"],
     ///             "keyLabourIDs":[]
+    ///         },
+    ///         {
+    ///             "keyCategoryID":"124",
+    ///             "categoryCode":"acme-brand",
+    ///             "keyCategoryTreeID":"2-BRANDS",
+    ///             "keyCategoryParentID":null,
+    ///             "name":"Paper Products",
+    ///             "description1":"View All Acme Brand's products",
+    ///             "description2":"Range shows all products made by the Acme corporation.",
+    ///             "description3":"",
+    ///             "description4":"",
+    ///             "metaTitle":"Acme Products",
+    ///             "metaKeywords":"",
+    ///             "metaDescription":"",
+    ///             "ordering": 1,
+    ///             "keyProductIDs":["PROD-003","PROD-004"],
+    ///             "keyDownloadIDs":[],
+    ///             "keyLabourIDs":[]
     ///         }
     ///     ]
     /// }
@@ -73,8 +110,13 @@ namespace EcommerceStandardsDocuments
     [DataContract]
     public class ESDocumentCategory : ESDocument
     {
-        /// <summary>List of category records</summary>
+        /// <summary>List of category tree records</summary>
         [JsonProperty(Order = -4)]
+        [DataMember]
+        public ESDRecordCategoryTree[] categoryTreeRecords;
+
+        /// <summary>List of category records</summary>
+        [JsonProperty(Order = -3)]
         [DataMember]
         public ESDRecordCategory[] dataRecords;
 
@@ -89,6 +131,28 @@ namespace EcommerceStandardsDocuments
         {
             this.resultStatus = resultStatus;
             this.message = message;
+            this.categoryTreeRecords = new ESDRecordCategoryTree[0];
+            this.dataRecords = categoryRecords;
+            this.configs = configs;
+            if (categoryRecords != null)
+            {
+                this.totalDataRecords = categoryRecords.Length;
+            }
+        }
+
+        /// <summary>Constructor</summary>
+        /// <param name="resultStatus">status of obtaining the category data</param>
+        /// <param name="message">message to accompany the result status</param>
+        /// <param name="categoryTreeRecords">list of category tree records</param>
+        /// <param name="categoryRecords">list of category records</param>
+        /// <param name="configs">A list of key value pairs that contain additional information about the document.
+        /// Ensure that a key "dataFields" exists that contains a comma delimited list of the category record properties that have data set. This advises systems processing the data which properties should be read and have defaults set if not included in each record.
+        /// </param>
+        public ESDocumentCategory(int resultStatus, string message, ESDRecordCategoryTree[] categoryTreeRecords, ESDRecordCategory[] categoryRecords, Dictionary<string, string> configs)
+        {
+            this.resultStatus = resultStatus;
+            this.message = message;
+            this.categoryTreeRecords = categoryTreeRecords;
             this.dataRecords = categoryRecords;
             this.configs = configs;
             if (categoryRecords != null)
